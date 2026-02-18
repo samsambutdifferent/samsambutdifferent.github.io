4f2962d646df31d81ca7e394e1a84f5774a4894b
samsambutdifferent <samuel.g.brightman@gmail.com>
2026-02-18 08:14:46 -0700

    Deploy FSI terminal updates

--- assets/mockData-BuIqxkcj.js
+++
@@ -1,6 +0,0 @@
-import{n as a}from"./index-C8H5gjM8.js";/**
- * @license lucide-react v0.303.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const e=a("ArrowUp",[["path",{d:"m5 12 7-7 7 7",key:"hav0vg"}],["path",{d:"M12 19V5",key:"x0mq9r"}]]),d={uploadPerformance:{ddn:2.1,aws:38},ttfb:{ddn:.052,aws:.523},iterations:1e3,speedup:{upload:18,ttfb:10},latencyP99:{ddn:12,aws:89}};export{e as A,d as M};
